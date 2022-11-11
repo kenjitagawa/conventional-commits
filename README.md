deee947982c5d2b058dbc5739111d31225d3e25e
Kenji Tagawa <kenji@tagawa.ca>
2022-11-11 10:41:09 -0500

    testings
    asdfasdf
    
    jira-issue: [kt-123]

--- README.md
+++ README.md
@@ -2,6 +2,70 @@
 
 ## Conventional Commit CLI Helper - PoC
 
-This is a conventional commits helper for the CLI. This is not 
-my code, it is a tutorial done by Charmbracelet on the subject of 
-[gum](https://github.com/charmbracelet/gum). 
+This is a conventional commits helper for the CLI. This is not
+my code, it is a tutorial done by Charmbracelet on the subject of
+[gum](https://github.com/charmbracelet/gum).
+
+## Installation
+
+
+```bash
+# macOS or Linux
+brew install gum
+
+# Arch Linux (btw)
+pacman -S gum
+
+# Nix
+nix-env -iA nixpkgs.gum
+# Or, with flakes
+nix run "github:charmbracelet/gum" -- --help
+
+# Debian/Ubuntu
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install gum
+
+# Fedora/RHEL
+echo '[charm]
+name=Charm
+baseurl=https://repo.charm.sh/yum/
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
+sudo yum install gum
+
+# Alpine
+apk add gum
+
+# Android (via termux)
+pkg install gum
+
+# Or with Go
+go install github.com/charmbracelet/gum@latest
+```
+---
+
+You can also download the binaries from the [releases page](https://github.com/charmbracelet/gum/releases), or build from source:
+
+```bash
+git clone https://github.com/charmbracelet/gum
+
+```
+
+
+:art:
+
+:zap:
+:fire:
+:bug:
+:ambulance:
+:sparkles:
+:memo:
+:rocket:
+:lipstick:
+:tada:
+:white_check_mark:
+:lock:
+:poop:
--- git-commit.sh
+++ git-commit.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
+function gitadd () {
+    FILESTOADD=$(ls | gum choose --no-limit)
+
+    gum confirm "Add these files?" && git add $FILESTOADD || exit 1
+    # printf '%s\n' "$1" >&2 ## Send message to stderr.
+    # exit "${2-1}" ## Return a code specified by $2, or 1 by default.
+}
+
+gum confirm "Did you make sure to run 'git add' on all the files you want to commit?" && echo "Cool! Continuing..." || \
+    (echo "Please run 'git add' on the files you want to commit, then run this script again." && gitadd)
+
+
 TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
 SCOPE=$(gum input --placeholder "scope")
 
-GITMOJI=$(gum choose ":art:" ":zap:" ":fire:" ":bug:" ":ambulance:" ":sparkles:" ":memo:" ":rocket" ":lipstick:" ":tada:" ":white_check_mark:" ":lock:" ":poop:" )
+# GITMOJI="$(gum table < gitmojis.csv -w 12,2 | cut -d ',' -f 1 | gum choose)"
+
+# GITMOJI=$(gum choose ":art:" ":zap:" ":fire:" ":bug:" ":ambulance:" ":sparkles:" ":memo:" ":rocket" ":lipstick:" ":tada:" ":white_check_mark:" ":lock:" ":poop:" )
+
+GITMOJI=$(echo ":art:" ":zap:" ":fire:" ":bug:" ":ambulance:" ":sparkles:" ":memo:" ":rocket" ":lipstick:" ":tada:" ":white_check_mark:" ":lock:" ":poop:" | gum format -t emoji | gum choose  )
+
 
 # Since the scope is optional, wrap it in parentheses if it has a value.
 test -n "$SCOPE" && SCOPE="($SCOPE)"
@@ -15,7 +32,11 @@ DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)
 
 MESSAGE="$TYPE$SCOPE: $GITMOJI $SUMMARY"
 
-JIRAISSUE=$(gum input --value "jira-issue: " --placeholder "Jira issue")
+JIRAISSUE=$(gum input --placeholder "Jira issue without the brackets.\
+            We will add that for you!")
+
+JIRAISSUEFORMATTED="jira-issue: [$JIRAISSUE]"
 
 # Commit these changes
-gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION" -m "$JIRAISSUE"
+gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION" -m "$JIRAISSUEFORMATTED"
+# gum confirm "Commit changes?" && echo "$MESSAGE\n$DESCRIPTION\n$JIRAISSUEFORMATTED"
